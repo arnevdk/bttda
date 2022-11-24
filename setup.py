f0f683243868317accf85d789cdc2cf72ede0bc9
Arne Van Den Kerchove <arne@vandenkerchove.com>
2022-11-24 13:47:15 +0100

    Create package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="hoda",
+    version="0.0.1",
+    description="Reguralized Higher Order Discriminant Analysis",
+    url="https://gitlab.kuleuven.be/compneuro/hoda-bci",
+    author="Arne Van Den Kerchove",
+    author_email="arne.vandenkerchove@kuleuven.be",
+    packages=["hoda"],
+    install_requires=["setuptools", "sklearn", "tensorly"],
+)
