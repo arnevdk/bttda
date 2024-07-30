c3c06c723784eb3ecda78e8a33af206f3a732c6f
Arne Van Den Kerchove <arne@vandenkerchove.com>
2024-07-30 21:29:17 +0200

    forward fitting on non-centered data with class weights

--- src/hoda/classification.py
+++ src/hoda/classification.py
@@ -1,16 +1,17 @@
 import numpy as np
 import tensorly as tl
 from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
-from sklearn.feature_selection import f_classif
 from sklearn.preprocessing import FunctionTransformer
-from hoda.backend import std
 
+from hoda.backend import std
 from hoda.tensorize import vec
+from hoda.util import f_oneway
 
 try:
     from toeplitzlda.classification import ToeplitzLDA
 except ImportError:
     pass
+
 
 class ZScore(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
@@ -18,15 +19,16 @@ class ZScore(BaseEstimator, TransformerMixin):
         self.std_ = std(X, axis=0)
         return self
 
-    def transform(self, X,y=None):
-        return (X - self.mean_)/self.std_
+    def transform(self, X, y=None):
+        return (X - self.mean_) / self.std_
+
 
 class SelectF(BaseEstimator, TransformerMixin):
     def __init__(self, alpha=0.5):
         self.alpha = alpha
 
     def fit(self, X, y=None):
-        self.F_, self.p_ = f_classif(X, y)
+        self.F_, self.p_ = f_oneway(X, y)
         self.mask_ = self.p_ < self.alpha
         if not np.any(self.mask_):
             self.mask_[np.argmax(self.F_)] = True
