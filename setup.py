7a9aa03806be80b1a8d1f34c8c88fe6f06703d32
Arne Van Den Kerchove <arne.vandenkerchove@kuleuven.be>
2024-08-30 11:52:13 +0200

    Fix setup

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     install_requires=["scikit-learn", "tensorly", "tqdm"],
     extras_require={
         "experiments": [
-            "mne<=1.6.1",
-            "pyyaml<5.4",
+            #"mne<=1.6.1",
+            #"pyyaml<5.4",
             "moabb==1.1.0",
             "matplotlib<3.9",
             "jupyter",
