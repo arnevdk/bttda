494004d7bc6e2144c699d4382068a811f3af2b0d
Arne Van Den Kerchove <arne@vandenkerchove.com>
2024-09-02 16:12:27 +0200

    Cross val predict

--- src/hoda/hoda.py
+++ src/hoda/hoda.py
@@ -775,7 +775,7 @@ class GreedyBTTDA(BTTDA):
         all_idc = np.arange(len(X))
         if test:
             idc, test_idc, _, _ = train_test_split(
-                all_idc, y, test_size=0.2, shuffle=True, random_state=42
+                all_idc, y, test_size=0.2, shuffle=True, random_state=3
             )
             splits = list(cv.split(idc, y[idc]))
             for f, (train_idc, val_idc) in enumerate(splits):
@@ -828,9 +828,9 @@ class GreedyBTTDA(BTTDA):
                     Xt = fold_Xt[fold]
                     clf = clone(self.clf).fit(Xt[train_idc], y[train_idc])
                     y_pred = clf.predict_proba(Xt)[:, 1]
-                    # y_pred = cross_val_predict(
-                    #    clf, Xt, y, n_jobs=-1, method="predict_proba"
-                    # )[:, 1]
+                    y_pred = cross_val_predict(
+                        clf, Xt, y, n_jobs=-1, method="predict_proba"
+                    )[:, 1]
                     train_score = roc_auc_score(y[train_idc], y_pred[train_idc])
                     val_score = roc_auc_score(y[val_idc], y_pred[val_idc])
                     val_scores[fold, ri] = val_score
